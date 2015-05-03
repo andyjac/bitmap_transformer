4c64f8a22b4b485011f731361bf8f69689d5eba2
Andrew Jacobson <andyjacuw@gmail.com>
2015-05-02 20:12:52 -0700

    Get header test passing

--- test/get-header-test.js
+++ test/get-header-test.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var getHeader = require('../lib/bitmap').getHeader;
+var getHeader = require('../lib/get-header');
 var expect = require('chai').expect;
 var data;
 
 describe('getHeader', function () {
-	it('should return the header', function () {
-		data = new Buffer(54);
-		data.write('BM');
-		expect(getHeader(data).head).to.eql('BM');
-	});
+  it('should return the header', function () {
+    data = new Buffer(54);
+    data.write('BM');
+    expect(getHeader(data).head).to.eql('BM');
+  });
 });
-
