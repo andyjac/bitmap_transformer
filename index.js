f9f1507d37b7a526df0b3cfa635c71cd27004253
Andrew Jacobson <andyjacuw@gmail.com>
2015-05-03 17:07:25 -0700

    Refactor blueTint to take a tint amount value

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var file = process.argv[2];
-var transform = process.argv[3];
+var options = {
+  file: process.argv[2],
+  transform: process.argv[3],
+  amount: process.argv[4]
+};
 
-module.exports = require('./lib/main')(file, transform);
+module.exports = require('./lib/main')(options);
